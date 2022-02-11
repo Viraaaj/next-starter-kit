48d16905769345619c33ef30adf7a3c56aa02f75
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2022-02-11 18:40:50 +0530

    Updated material Ui themes, installed packages, _document.js, _app.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next Starter</title>
+        <meta name="description" content="Next Starter Kit" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Happy Development!
+    </div>
+  );
+}
